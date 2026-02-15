9be8ffa897c34f056af5cd597594b7086a67f399
bilal-hayat <bilalhayat1@gmail.com>
2026-02-15 21:08:28 +0530

    changes for caching

--- SpringBoot-Ollama-Rag-Example/src/main/java/com/example/rag/OllamaFileChatApplication.java
+++ SpringBoot-Ollama-Rag-Example/src/main/java/com/example/rag/OllamaFileChatApplication.java
@@ -3,8 +3,10 @@ package com.example.rag;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class OllamaFileChatApplication {
 
     public static void main(String[] args) {
--- SpringBoot-Ollama-Rag-Example/src/main/java/com/example/rag/service/OllamaService.java
+++ SpringBoot-Ollama-Rag-Example/src/main/java/com/example/rag/service/OllamaService.java
@@ -1,6 +1,8 @@
 package com.example.rag.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -8,6 +10,7 @@ import org.springframework.web.client.RestTemplate;
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 @Service
 public class OllamaService {
 
@@ -19,13 +22,17 @@ public class OllamaService {
 
     private final RestTemplate restTemplate = new RestTemplate();
 
+    @Cacheable(value = "response", key ="#question")
     public String askQuestion(String context, String question) {
+
+        log.info("==== inside askQuestion ==== ");
 
         String prompt = """
         Answer the question using ONLY the information below.
         If the answer is not present, say "I don't know".
 
         Context:
+        
         %s
 
         Question:
